887afea4d002caca5fc0a0c8541a9e1f3a2943de
flavia-webster <75635385+flavia-webster@users.noreply.github.com>
2022-11-27 19:02:57 -0300

    Update Treinador.java

--- pooAlgumasPartes/Treinador.java
+++ pooAlgumasPartes/Treinador.java
@@ -1,11 +1,75 @@
 public class Treinador extends Card{
     private int idEfeito;
+    private Ataque tipo;
+    private int cura;
 
-    public Treinador(String nome,String anId,String anImageId,int idEfeito)
+
+
+    //carta e ação
+
+    public Treinador(String nome,String anId,String anImageId, Ataque tipo, int idEfeito)
     {
         super(nome,anId,anImageId);
-        this.idEfeito = idEfeito;
+        this.tipo = tipo;
     }
 
-    public int getIdEfeito() {return idEfeito;}
+
+    // carta efeito cura
+
+    public Treinador(String nome, String anId, String anImageId, int cura)
+    {
+        super(nome, anId, anImageId);
+        tipo = Ataque.cura;
+        this.cura = cura;
+        
+    }
+
+
+
+    public Acao getTipo() {
+        return tipo;
+    }
+
+    public int getCura() {
+        return cura;
+    }
+
+    public String toString() {
+        return "TREINADOR -- " + getNome() + " : " + tipo + cura;
+    }
+
+    public int getIdEfeito(){
+        return idEfeito;
+    }
+
+
+    // troca pelo que estiver mais à esquerda no baralho
+    public void substituicao(CardDeck banco, CardDeck ativo, CardDeck mao) {
+
+        if (banco.getCards().size() != 0) 
+        {
+            Card c = banco.getBaralho().get(0);
+            for (Card card : ativo.getBaralho()) 
+            {
+                if (card instanceof Pokemon) 
+                {
+                    Pokemon p = (Pokemon) card;
+                    if (p.getGeracaoAnterior()!=null) 
+                    {
+                        mao.getBaralho().add(p);
+                    } else
+                        banco.getBaralho().add(p);
+                }
+            }
+            banco.getBaralho().remove(null);
+
+            ativo.getBaralho().clear();
+
+            ativo.getBaralho().add(c);
+        } else 
+        {
+            System.out.println("Baralho vazio");
+        }
+    }
+
 }
